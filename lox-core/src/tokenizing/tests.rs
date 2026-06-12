10305c7b9e58b7599d9485ac6143ccca12fa09f5
haraldmaida <harald.maida@innoave.com>
2026-06-12 14:34:11 +0200

    refactor: rearrange tests for tokenizing related types

--- lox-core/src/tokenizing/tests.rs
+++ lox-core/src/tokenizing/tests.rs
@@ -2,6 +2,119 @@ use super::{Location, *};
 use asserting::prelude::*;
 use std::io::{BufReader, Cursor};
 
+mod lexing_error_code {
+    use super::*;
+
+    #[test]
+    fn can_be_converted_from_io_error() {
+        let code = LexingErrorCode::from(io::Error::from(io::ErrorKind::UnexpectedEof));
+
+        assert_that!(code).is_equal_to(LexingErrorCode::IoError(
+            "unexpected end of file".to_string(),
+        ));
+    }
+}
+
+mod lexing_error {
+    use super::*;
+
+    #[test]
+    fn display_format_io_error() {
+        let error = LexingError {
+            code: LexingErrorCode::IoError("I/O error".to_string()),
+            location: Location { line: 1, char: 0 },
+        };
+
+        assert_that!(error.to_string()).is_equal_to("I/O error at 1:0");
+    }
+
+    #[test]
+    fn display_format_unexpected_character() {
+        let error = LexingError {
+            code: LexingErrorCode::UnexpectedCharacter('§'),
+            location: Location { line: 74, char: 23 },
+        };
+
+        assert_that!(error.to_string()).is_equal_to("unexpected character '§' at 74:23");
+    }
+}
+
+mod token {
+    use super::*;
+
+    #[test]
+    fn debug_format_end_of_file() {
+        let token = Token::EndOfFile;
+
+        let debug_string = format!("{token:?}");
+
+        assert_that!(debug_string).is_equal_to("EOF  null");
+    }
+
+    #[test]
+    fn debug_format_comma() {
+        let token = Token::Comma;
+
+        let debug_string = format!("{token:?}");
+
+        assert_that!(debug_string).is_equal_to("COMMA , null");
+    }
+
+    #[test]
+    fn debug_format_left_paren() {
+        let token = Token::LeftParen;
+
+        let debug_string = format!("{token:?}");
+
+        assert_that!(debug_string).is_equal_to("LEFT_PAREN ( null");
+    }
+
+    #[test]
+    fn debug_format_right_paren() {
+        let token = Token::RightParen;
+
+        let debug_string = format!("{token:?}");
+
+        assert_that!(debug_string).is_equal_to("RIGHT_PAREN ) null");
+    }
+
+    #[test]
+    fn debug_format_left_brace() {
+        let token = Token::LeftBrace;
+
+        let debug_string = format!("{token:?}");
+
+        assert_that!(debug_string).is_equal_to("LEFT_BRACE { null");
+    }
+
+    #[test]
+    fn debug_format_right_brace() {
+        let token = Token::RightBrace;
+
+        let debug_string = format!("{token:?}");
+
+        assert_that!(debug_string).is_equal_to("RIGHT_BRACE } null");
+    }
+
+    #[test]
+    fn debug_format_dot() {
+        let token = Token::Dot;
+
+        let debug_string = format!("{token:?}");
+
+        assert_that!(debug_string).is_equal_to("DOT . null");
+    }
+
+    #[test]
+    fn debug_format_semicolon() {
+        let token = Token::Semicolon;
+
+        let debug_string = format!("{token:?}");
+
+        assert_that!(debug_string).is_equal_to("SEMICOLON ; null");
+    }
+}
+
 #[test]
 fn can_tokenize_str() {
     let mut source_code = "()";
@@ -238,117 +351,4 @@ fn tokenize_string_literal_which_is_unterminated() {
         }),
         Ok(Token::EndOfFile),
     ]);
-}
-
-mod lexing_error_code {
-    use super::*;
-
-    #[test]
-    fn can_be_converted_from_io_error() {
-        let code = LexingErrorCode::from(io::Error::from(io::ErrorKind::UnexpectedEof));
-
-        assert_that!(code).is_equal_to(LexingErrorCode::IoError(
-            "unexpected end of file".to_string(),
-        ));
-    }
-}
-
-mod lexing_error {
-    use super::*;
-
-    #[test]
-    fn display_format_io_error() {
-        let error = LexingError {
-            code: LexingErrorCode::IoError("I/O error".to_string()),
-            location: Location { line: 1, char: 0 },
-        };
-
-        assert_that!(error.to_string()).is_equal_to("I/O error at 1:0");
-    }
-
-    #[test]
-    fn display_format_unexpected_character() {
-        let error = LexingError {
-            code: LexingErrorCode::UnexpectedCharacter('§'),
-            location: Location { line: 74, char: 23 },
-        };
-
-        assert_that!(error.to_string()).is_equal_to("unexpected character '§' at 74:23");
-    }
-}
-
-mod token {
-    use super::*;
-
-    #[test]
-    fn debug_format_end_of_file() {
-        let token = Token::EndOfFile;
-
-        let debug_string = format!("{token:?}");
-
-        assert_that!(debug_string).is_equal_to("EOF  null");
-    }
-
-    #[test]
-    fn debug_format_comma() {
-        let token = Token::Comma;
-
-        let debug_string = format!("{token:?}");
-
-        assert_that!(debug_string).is_equal_to("COMMA , null");
-    }
-
-    #[test]
-    fn debug_format_left_paren() {
-        let token = Token::LeftParen;
-
-        let debug_string = format!("{token:?}");
-
-        assert_that!(debug_string).is_equal_to("LEFT_PAREN ( null");
-    }
-
-    #[test]
-    fn debug_format_right_paren() {
-        let token = Token::RightParen;
-
-        let debug_string = format!("{token:?}");
-
-        assert_that!(debug_string).is_equal_to("RIGHT_PAREN ) null");
-    }
-
-    #[test]
-    fn debug_format_left_brace() {
-        let token = Token::LeftBrace;
-
-        let debug_string = format!("{token:?}");
-
-        assert_that!(debug_string).is_equal_to("LEFT_BRACE { null");
-    }
-
-    #[test]
-    fn debug_format_right_brace() {
-        let token = Token::RightBrace;
-
-        let debug_string = format!("{token:?}");
-
-        assert_that!(debug_string).is_equal_to("RIGHT_BRACE } null");
-    }
-
-    #[test]
-    fn debug_format_dot() {
-        let token = Token::Dot;
-
-        let debug_string = format!("{token:?}");
-
-        assert_that!(debug_string).is_equal_to("DOT . null");
-    }
-
-    #[test]
-    fn debug_format_semicolon() {
-        let token = Token::Semicolon;
-
-        let debug_string = format!("{token:?}");
-
-        assert_that!(debug_string).is_equal_to("SEMICOLON ; null");
-    }
 }
