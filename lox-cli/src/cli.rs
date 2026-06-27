2a057d9bc797a392d3aa070052bcf209eb9836ae
haraldmaida <harald.maida@innoave.com>
2026-06-27 22:06:33 +0200

    feat: custom exit codes of CLI-program according the recommendations in the BSD <sysexits.h> specification

--- lox-cli/src/cli.rs
+++ lox-cli/src/cli.rs
@@ -1,3 +1,5 @@
+use std::process::{ExitCode, Termination};
+
 #[derive(clap::Parser, Debug)]
 #[command(name = "lox", version = "0.1.0", about = "A Lox interpreter")]
 pub struct Cli {
@@ -25,4 +27,48 @@ pub enum Command {
     },
     /// Start the REPL
     Repl,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum CliStatus {
+    /// The CLI command completed successfully
+    Success,
+    /// The source file specified by the user cannot be read
+    CannotReadSourceFile,
+    /// Writing an output file failed.
+    CannotWriteOutputFile,
+    /// The CLI command was used incorrectly.
+    /// (e.g., bad flag, missing argument, etc.)
+    InvalidArgument,
+    /// Failures that happen when running the user-provided program
+    /// (either in the interpreter or the VM)
+    RuntimeError,
+    /// Syntax error in the source code
+    SyntaxError,
+    /// An unrecoverable error in the system, e.g., in the parser, interpreter,
+    /// compiler, or VM.
+    ///
+    /// This is most likely a bug that needs to be fixed by the
+    /// developers
+    SystemError,
+}
+
+impl CliStatus {
+    pub const fn code(self) -> u8 {
+        match self {
+            Self::Success => 0,
+            Self::CannotReadSourceFile => 66,
+            Self::CannotWriteOutputFile => 73,
+            Self::InvalidArgument => 64,
+            Self::RuntimeError | Self::SyntaxError => 65,
+            Self::SystemError => 70,
+        }
+    }
+}
+
+impl Termination for CliStatus {
+    fn report(self) -> ExitCode {
+        ExitCode::from(self.code())
+    }
 }
--- lox-cli/src/main.rs
+++ lox-cli/src/main.rs
@@ -10,91 +10,187 @@ mod dummy_extern_uses {
 mod cli;
 mod repl;
 
-use crate::cli::{Cli, Command};
+use crate::cli::{Cli, CliStatus, Command};
 use clap::Parser;
 use lox_core::ast_printer::AstPrinter;
 use lox_core::interpreter::Interpreter;
 use lox_core::parse::Parse;
-use lox_core::program::{IntoProgram, ProgramError};
+use lox_core::program::{IntoProgram, Program, ProgramError};
 use lox_core::resolver::Resolve;
 use lox_core::runtime::RuntimeContext;
 use lox_core::tokenize::Tokenize;
-use miette::{IntoDiagnostic, NamedSource, Report, WrapErr};
+use miette::{NamedSource, miette};
+use std::io::{Stderr, Stdout};
+use std::ops::ControlFlow;
+use std::ops::ControlFlow::{Break, Continue};
 use std::path::Path;
 use std::{fs, io};
 
-fn main() -> Result<(), miette::Error> {
-    let cli = Cli::parse();
+fn main() -> CliStatus {
+    let cli = match parse_cli_args() {
+        Continue(args) => args,
+        Break(exit_status) => return exit_status,
+    };
 
     let mut stdout = io::stdout();
     let mut stderr = io::stderr();
 
     match &cli.command {
         Command::Tokenize { source } => {
-            let source_code = read_source_file(source)?;
-            source_code.tokenize().for_each(|item| match item {
-                Ok(token) => println!("{token:#}"),
-                Err(error) => {
-                    let error = Report::from(error)
-                        .with_source_code(NamedSource::new(source, source_code.clone()));
-                    eprintln!("\n{error}\n");
+            let source_code = match read_source_file(source) {
+                Continue(code) => code,
+                Break(exit_status) => {
+                    return exit_status;
                 },
-            });
+            };
+            tokenize_source_code(source, &source_code)
         },
         Command::Parse { source } => {
-            let source_code = read_source_file(source)?;
-            match source_code.tokenize().parse_expr() {
-                Ok(ast) => {
-                    let mut output = String::new();
-                    let mut rtc = RuntimeContext::new(&mut stdout, &mut stderr)
-                        .with_fancy_error_messages(Some(source), &source_code);
-                    AstPrinter::print(&mut rtc, &ast, &mut output)?;
-                    println!("{output}");
-                },
-                Err(error) => {
-                    let error =
-                        Report::from(error).with_source_code(NamedSource::new(source, source_code));
-                    eprintln!("{error:?}");
-                },
-            }
+            let source_code = match read_source_file(source) {
+                Continue(code) => code,
+                Break(exit_status) => return exit_status,
+            };
+            parse_and_print_ast(&mut stdout, &mut stderr, source, &source_code)
         },
         Command::Interpret { source } => {
-            let source_code = read_source_file(source)?;
-            match source_code
-                .tokenize()
-                .parse()
-                .map_err(|errors| errors.into_iter().map(ProgramError::from).collect())
-                .and_then(|statements| {
-                    statements
-                        .resolve()
-                        .map_err(|errors| errors.into_iter().map(ProgramError::from).collect())
-                })
-                .into_program()
-            {
-                Ok(program) => {
-                    let mut interpreter = Interpreter::default();
-                    let mut rtc = RuntimeContext::new(&mut stdout, &mut stderr)
-                        .with_fancy_error_messages(Some(source), &source_code);
-                    interpreter.interpret(&mut rtc, &program);
+            let source_code = match read_source_file(source) {
+                Continue(code) => code,
+                Break(exit_status) => return exit_status,
+            };
+            let program = match parse_program(source, &source_code) {
+                Continue(program) => program,
+                Break(exit_status) => return exit_status,
+            };
+            interpret_program(&mut stdout, &mut stderr, source, &source_code, &program)
+        },
+        Command::Repl => match repl::run(io::stdin(), &mut stdout, &mut stderr) {
+            Ok(()) => CliStatus::Success,
+            Err(error) => {
+                eprintln!("{error:?}");
+                CliStatus::SystemError
+            },
+        },
+    }
+}
+
+fn parse_cli_args() -> ControlFlow<CliStatus, Cli> {
+    match Cli::try_parse() {
+        Ok(args) => Continue(args),
+        Err(clap_err) => {
+            if clap_err.use_stderr() {
+                clap_err
+                    .print()
+                    .expect("failed to print error message from parsing the CLI arguments");
+                Break(CliStatus::InvalidArgument)
+            } else {
+                clap_err
+                    .print()
+                    .expect("failed to print help message for CLI usage");
+                Break(CliStatus::Success)
+            }
+        },
+    }
+}
+
+fn read_source_file(source_file: impl AsRef<Path>) -> ControlFlow<CliStatus, String> {
+    let source_file = source_file.as_ref();
+    match fs::read_to_string(source_file) {
+        Ok(source_code) => Continue(source_code),
+        Err(error) => {
+            let report = miette!(error).wrap_err(format!(
+                "failed to read source file {}",
+                source_file.display()
+            ));
+            eprintln!("{report:?}");
+            Break(CliStatus::CannotReadSourceFile)
+        },
+    }
+}
+
+fn tokenize_source_code(source: &str, source_code: &str) -> CliStatus {
+    let mut cli_status = CliStatus::Success;
+    for item in source_code.tokenize() {
+        match item {
+            Ok(token) => {
+                println!("{token:#}");
+            },
+            Err(error) => {
+                let error = miette!(error)
+                    .with_source_code(NamedSource::new(source, source_code.to_owned()));
+                eprintln!("{error:?}");
+                cli_status = CliStatus::SyntaxError;
+            },
+        }
+    }
+    cli_status
+}
+
+fn parse_and_print_ast(
+    mut stdout: &mut Stdout,
+    mut stderr: &mut Stderr,
+    source: &str,
+    source_code: &str,
+) -> CliStatus {
+    match source_code.tokenize().parse_expr() {
+        Ok(ast) => {
+            let mut output = String::new();
+            let mut rtc = RuntimeContext::new(&mut stdout, &mut stderr)
+                .with_fancy_error_messages(Some(source), source_code);
+            match AstPrinter::print(&mut rtc, &ast, &mut output) {
+                Ok(()) => {
+                    println!("{output}");
+                    CliStatus::Success
                 },
-                Err(syntax_errors) => {
-                    for error in syntax_errors {
-                        let error = Report::from(error)
-                            .with_source_code(NamedSource::new(source, source_code.clone()));
-                        eprintln!("{error:?}");
-                    }
+                Err(error) => {
+                    let error = miette!(error);
+                    eprintln!("{error:?}");
+                    CliStatus::SystemError
                 },
             }
         },
-        Command::Repl => repl::run(io::stdin(), &mut stdout, &mut stderr).into_diagnostic()?,
+        Err(error) => {
+            let error =
+                miette!(error).with_source_code(NamedSource::new(source, source_code.to_owned()));
+            eprintln!("{error:?}");
+            CliStatus::SyntaxError
+        },
     }
-
-    Ok(())
 }
 
-fn read_source_file(source_file: impl AsRef<Path>) -> Result<String, miette::Error> {
-    let source_file = source_file.as_ref();
-    fs::read_to_string(source_file)
-        .into_diagnostic()
-        .wrap_err_with(|| format!("failed to read source file {}", source_file.display()))
+fn parse_program(source: &str, source_code: &str) -> ControlFlow<CliStatus, Program> {
+    match source_code
+        .tokenize()
+        .parse()
+        .map_err(|errors| errors.into_iter().map(ProgramError::from).collect())
+        .and_then(|statements| {
+            statements
+                .resolve()
+                .map_err(|errors| errors.into_iter().map(ProgramError::from).collect())
+        })
+        .into_program()
+    {
+        Ok(program) => Continue(program),
+        Err(syntax_errors) => {
+            for error in syntax_errors {
+                let error = miette!(error)
+                    .with_source_code(NamedSource::new(source, source_code.to_owned()));
+                eprintln!("{error:?}");
+            }
+            Break(CliStatus::SyntaxError)
+        },
+    }
+}
+
+fn interpret_program(
+    mut stdout: &mut Stdout,
+    mut stderr: &mut Stderr,
+    source: &str,
+    source_code: &str,
+    program: &Program,
+) -> CliStatus {
+    let mut interpreter = Interpreter::default();
+    let mut rtc = RuntimeContext::new(&mut stdout, &mut stderr)
+        .with_fancy_error_messages(Some(source), source_code);
+    interpreter.interpret(&mut rtc, program);
+    CliStatus::Success
 }
